ad80ac886291b65dd9fe948327df679144ee1ab2
yakup karakuş <ykp_krk@hotmail.com>
2022-07-29 19:19:50 +0300

    learned some of string functions

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 
+phrase="Yakup is learning Pyhton.\n I am very exited."
+print(phrase.lower())
+print(phrase.upper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("lear"))
+print(phrase.replace("Yakup", "Ali"))
