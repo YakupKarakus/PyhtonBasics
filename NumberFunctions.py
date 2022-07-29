741fd157cf4f289e9544fcb714396b90ed19fecf
yakup karakuş <ykp_krk@hotmail.com>
2022-07-29 19:58:05 +0300

    NumberFunctions pyhton file added.

---
+++ NumberFunctions.py
@@ -0,0 +1,17 @@
+from math import *
+print(2)
+print(2+3.5)
+print(2+3.5*2)
+print(10%3)
+
+number=2
+print(str(number)+" is my favorite number")
+negativenumber=-5
+print(abs(negativenumber))
+print(pow(5,2))
+print(max(5,2))
+print(round(2.6))
+print(floor(3.5))
+print(ceil(3.5))
+print(sqrt(3.5))
+
